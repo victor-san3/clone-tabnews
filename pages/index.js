d1333be6fc5580fd5f5c9066ed7d25c028538067
Victor Sanchez <victor.san@hotmail.com>
2026-07-25 19:44:35 +0000

    adiciona uma imagem do hexagrama unicursal ao fim da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function Home() {
-    return <h1>Beauty and strength, leaping laughter and delicious languor, force and fire, are of us.</h1>
+  return (
+    <h1>
+      Beauty and strength, leaping laughter and delicious languor, force and fire, are of us.{" "}
+      <svg
+        viewBox="0 0 194.21 221"
+        style={{
+          display: "inline-block",
+          height: "1em",
+          verticalAlign: "-0.15em",
+          marginLeft: "0.2em",
+        }}
+      >
+        <path
+          d="M96.853 20.5l74.531 127.929L22.321 72.57 96.853 200.5l74.531-127.929L22.321 148.43 96.853 20.5z"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="12"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </h1>
+  );
 }
 
 export default Home;
